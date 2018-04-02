e2e172275ffb246906dac0f1fff934fd9fec0e60
Mihir Kumar <mkumar12@illinois.edu>
2018-04-02 15:34:53 -0500

    Adding lib files and injected script

--- testfile.js
+++ testfile.js
@@ -1,3 +1,8 @@
+// import './oaa_a11y_evaluation';
+// import './oaa_a11y_rulesets';
+// import './oaa_a11y_rules';
+
+"use strict";
 var fs = require('fs');
 var system = require('system');
 var args = system.args;
@@ -8,6 +13,27 @@ var page = require('webpage').create();
 var url = 'http://www.mihirkumar.com';
 
 page.open(url, function(status) {
+
+    page.injectJs('oaa_a11y_evaluation.js');
+    page.injectJs('oaa_a11y_rules.js');
+    page.injectJs('oaa_a11y_rulesets.js');
+    console.log('injected');
+
+    //Required script starts
+    var ruleset = OpenAjax.a11y.RulesetManager.getRuleset("ARIA_STRICT");
+    var evaluator_factory = OpenAjax.a11y.EvaluatorFactory.newInstance();
+    evaluator_factory.setParameter('ruleset', ruleset);
+    evaluator_factory.setFeature('eventProcessing', 'fae-util');
+    evaluator_factory.setFeature('groups', 7);
+    var evaluator = evaluator_factory.newEvaluator();
+
+    //Need to know doc.location.href equivalent of phantomJS
+    var evaluation = evaluator.evaluate(doc, page.title, doc.location.href);
+    var out = evaluation.toJSON(true);
+    //Required script ends
+
+    console.log('line 30');
+
     var links = page.evaluate(function() {
         var anchorElements = document.getElementsByTagName('a');
 
@@ -20,12 +46,9 @@ page.open(url, function(status) {
         return linkArray;
     });
 
-    //Possible optimization by taking care of this in the for loop, old implementation
-    // linkArray = links.join('\n');
-    //    page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js", function(){
-    //  console.log(links.serializeArray());
-    // });
-
-    fs.write(writeToFile, JSON.stringify(links), 'w');
+    //Possible optimization by taking care of this in the for loop
+    linkArray = links.join('\n');
+    console.log('line 38');
+    fs.write(writeToFile, linkArray, 'w');
     phantom.exit();
 });
